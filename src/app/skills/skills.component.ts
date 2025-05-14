e1e16cb096d07dfd7c0d19e01f8194826abec131
Szilard <szilard.fer@gmail.com>
2025-05-14 16:55:52 +0200

    Refactoring css

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -5,38 +5,10 @@ import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-skills',
-  imports: [CarouselModule, CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-  public customOptions: OwlOptions = {
-    loop: true,
-    autoplaySpeed: 200,
-    autoplay: true,                            
-    animateIn: true,
-    mouseDrag: true,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 200,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1
-      },
-      350: {
-        items: 2
-      },
-      550: {
-        items: 3
-      },
-      800: {
-        items: 4
-      },
-      1000: {
-        items: 5
-      }
-    }
-  }
+  
 }
