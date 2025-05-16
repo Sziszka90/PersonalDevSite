b1ffc8f51eebe8e07b1d658fbd183943fe5f33ed
Szilard <szilard.fer@gmail.com>
2025-05-16 20:55:08 +0200

    Refactoring, cleaning code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 import { provideAnimations, provideNoopAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
     provideAnimations(), 
     provideNoopAnimations()
   ]
