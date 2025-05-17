fb5db223bd7e8bd3be95ec392315bcaa17db1d93
Szilard <szilard.fer@gmail.com>
2025-05-17 12:37:24 +0200

    Fix img sources

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -10,15 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class SkillsComponent {
 
   positionedSkills = [
-    { name: '.NET', img: '../../assets/NET core.png' },
-    { name: 'Angular', img: '../../assets/Angular.png' },
-    { name: 'VisualStudio', img: '../../assets/Visual Studio.png' },
-    { name: 'Azure', img: '../../assets/Azure.png' },
-    { name: 'MSSQL', img: '../../assets/Microsoft SQL Server.png' },
-    { name: 'Oracle', img: '../../assets/Oracle.png' },
-    { name: 'RabbitMQ', img: '../../assets/RabbitMQ.png' },
-    { name: 'Docker', img: '../../assets/Docker.png' },
-    { name: 'Kubernetes', img: '../../assets/Kubernetes.png' },
-    { name: 'Helm', img: '../../assets/Helm.png' },
+    { name: '.NET', img: 'assets/NET core.png' },
+    { name: 'Angular', img: 'assets/Angular.png' },
+    { name: 'VisualStudio', img: 'assets/Visual Studio.png' },
+    { name: 'Azure', img: 'assets/Azure.png' },
+    { name: 'MSSQL', img: 'assets/Microsoft SQL Server.png' },
+    { name: 'Oracle', img: 'assets/Oracle.png' },
+    { name: 'RabbitMQ', img: 'assets/RabbitMQ.png' },
+    { name: 'Docker', img: 'assets/Docker.png' },
+    { name: 'Kubernetes', img: 'assets/Kubernetes.png' },
+    { name: 'Helm', img: 'assets/Helm.png' },
   ];
 }
