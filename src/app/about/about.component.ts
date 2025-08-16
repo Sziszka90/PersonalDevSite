b9728ed63d546e8de1b151a0d634d6cf9606505b
Szilard Ferencz <szilard.fer@gmail.com>
2025-08-16 22:20:13 +0200

    Working on animation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,19 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { TypingAnimationDirective } from '../../directives/typewriter/typing-animation.directive';
-import { interval } from 'rxjs';
 import { FloatInOnScrollDirective } from '../directives/float-in.directive';
+import { CommonModule } from '@angular/common';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [TypingAnimationDirective, FloatInOnScrollDirective],
+  imports: [CommonModule, TypingAnimationDirective, FloatInOnScrollDirective],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
 export class AboutComponent implements OnInit {
   start: boolean = false;
 
+  showLearn = false;
+  showBuild = false;
+  showDeploy = false;
+  showDone = false;
+
   ngOnInit() {
     interval(5000).subscribe(() => this.start = !this.start);
+    this.startAnimationFlow();
+  }
+
+  startAnimationFlow() {
+    this.showLearn = true;
+    this.showBuild = false;
+    this.showDeploy = false;
+    this.showDone = false;
+    setTimeout(() => {
+      this.showLearn = false;
+      this.showBuild = true;
+      setTimeout(() => {
+        this.showBuild = false;
+        this.showDeploy = true;
+        setTimeout(() => {
+          this.showDeploy = false;
+          this.showDone = true;
+          setTimeout(() => {
+            this.showDone = false;
+            this.startAnimationFlow();
+          }, 3000);
+        }, 2500);
+      }, 2500);
+    }, 2500);
   }
 }
