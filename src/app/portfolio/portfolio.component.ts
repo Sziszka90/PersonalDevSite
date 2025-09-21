eec2e81e6c834718e378ab995f1dea0dc5be3731
Szilard Ferencz <szilard.fer@gmail.com>
2025-09-21 21:01:27 +0200

    Update

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { FloatInOnScrollDirective } from '../directives/float-in.directive';
-import { ImageModalComponent } from '../shared/image-modal.component';
+
 
 @Component({
   selector: 'app-portfolio',
@@ -12,16 +12,4 @@ import { ImageModalComponent } from '../shared/image-modal.component';
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent {
-  private matDialog = inject(MatDialog);
-
-  openImageModal(imageSrc: string, alt: string = '') {
-    this.matDialog.open(ImageModalComponent, {
-      data: { imageSrc, alt },
-      width: '90vw',
-      height: '90vh',
-      autoFocus: false,
-      restoreFocus: false
-    });
-  }
-}
+export class PortfolioComponent {}
